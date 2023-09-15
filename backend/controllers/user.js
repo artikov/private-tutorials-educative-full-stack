b30131bfb9d5e77af566816c50b30d39451d9016
artikov <oybekartikov@icloud.com>
2023-09-15 09:21:40 +0500

    email verification done

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,92 @@
 const { User, validate } = require("../models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { encrypt, decrypt } = require("../utils/confirmation");
+const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+
+const createTransporter = async () => {
+  const oauth2Client = new OAuth2(
+    "{{OAUTH_CLIENT_ID}}",
+    "{{OAUTH_CLIENT_SECRET}}",
+    "https://developers.google.com/oauthplayground"
+  );
+
+  oauth2Client.setCredentials({
+    refresh_Token: "{{OAUTH_REFRESH_TOKEN}}",
+  });
+
+  const accessToken = await new Promise((resolve, reject) => {
+    oauth2Client.getAccessToken((err, token) => {
+      if (err) {
+        reject();
+      }
+      resolve(token);
+    });
+  });
+
+  const Transport = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: "{{GMAIL_EMAIL}}",
+      accessToken,
+      clientId: "{{OAUTH_CLIENT_ID}}",
+      clientSecret: "{{OAUTH_CLIENT_SECRET}}",
+      refreshToken: "{{OAUTH_REFRESH_TOKEN}}",
+    },
+  });
+
+  return Transport;
+};
+
+const sendEmail = async ({ email, username, res }) => {
+  const confirmationToken = encrypt(username);
+  const apiUrl = process.env.API_URL || "http://0.0.0.0:4000";
+
+  const Transport = await createTransporter();
+
+  //configuring email options
+  const mailOptions = {
+    from: "Educative Fullstack Course",
+    to: email,
+    subject: "Email Confirmation",
+    html: `Press the following link to verify your email: <a href=${apiUrl}/confirmation/${confirmationToken}>Verification Link</a>`,
+  };
+
+  //sending email
+  Transport.sendMail(mailOptions, function (error, response) {
+    if (error) {
+      res.status(400).send(error);
+    } else {
+      res.status(201).json({
+        message: "Account created successfully please check your email.",
+      });
+    }
+  });
+};
+
+exports.verify = async (req, res) => {
+  try {
+    const { confirmationToken } = req.params;
+
+    const username = decrypt(confirmationToken);
+
+    const user = await User.findOne({ username: username });
+
+    if (user) {
+      user.isVerified = true;
+      await user.save();
+      res.status(200).send({ message: "Account verified successfully" });
+    } else {
+      return res.status(400).send({ message: "Invalid verification token" });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send(err);
+  }
+};
 
 exports.signup = async (req, res) => {
   try {
---
+++ backend/utils/confirmation.js
@@ -0,0 +1,32 @@
+const crypto = require("crypto");
+
+const algorithm = process.env.CRYPTO_ALGORITHM || "aes-256-ctr";
+const secretKey = process.env.CONFIRMATION_SECRET_KEY;
+const iv = process.env.INITIALIZATION_VECTOR;
+
+const encrypt = (token) => {
+  const cipher = crypto.createCipheriv(
+    algorithm,
+    secretKey,
+    Buffer.from(iv, "hex")
+  );
+  const encrypted = Buffer.concat([cipher.update(token), cipher.final()]);
+
+  return encrypted.toString("hex");
+};
+
+const decrypt = (hash) => {
+  const decipher = crypto.createDecipheriv(
+    algorithm,
+    secretKey,
+    Buffer.from(iv, "hex")
+  );
+  const decrpyted = Buffer.concat([
+    decipher.update(Buffer.from(hash, "hex")),
+    decipher.final(),
+  ]);
+
+  return decrpyted.toString();
+};
+
+module.exports = { encrypt, decrypt };
